a4028b95dea2cc395afea09d4cad2fcfb744e216
Juan Herrera <juanchoherrerac_27@hotmail.com>
2021-08-23 23:20:27 -0500

    adding chnages: Functions JS

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 // Agregaremos funciones y haremos el llamdo de una API para obtener datos y mostralos en pantalla
+
+fetch('https://www.etnassoft.com/api/v1/get/?id=589')
+.then(data => data.json())
+.then( data => {
+    
+})
