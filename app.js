d226fb4a53b43c6be0c7296731bf48e2546a5c9a
Juan Herrera <juanchoherrerac_27@hotmail.com>
2021-08-23 23:29:42 -0500

    adding changes: API-Books

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 // Agregaremos funciones y haremos el llamdo de una API para obtener datos y mostralos en pantalla
 
+posts = null;
+var contenedor = document.getElementById('h2')
+
+
 fetch('https://www.etnassoft.com/api/v1/get/?id=589')
 .then(data => data.json())
 .then( data => {
-    
+    data = posts
 })
+
+
+function mostrarDatos(posts) {
+
+    posts.map((post, i) => {
+        let titulo = document.createElement('h2');
+
+        titulo.innerHTML = (i + 1) + " - " + post.title;
+        contenido.innerHTML = post.body
+
+        contenedor.appendChild(titulo)
+
+ })
+
+}
